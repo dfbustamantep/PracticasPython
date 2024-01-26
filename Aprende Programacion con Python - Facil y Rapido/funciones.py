1e3e00a84cca4e190ace07cb3d9a1bcd7d2a9444
Daniel Bustamante <vtdanielf@gmail.com>
2024-01-26 10:11:01 +0100

    Funciones en python

---
+++ Aprende Programacion con Python - Facil y Rapido/funciones.py
@@ -0,0 +1,8 @@
+def Saludar (nombre):
+    #print(f"Hola {nombre}")
+    #cuando se usa la coma el lenguaje agrega un espacio
+    #print("Hola ",nombre)
+    print("Hola "+nombre)
+
+nombre = input("Digite su nombre ")
+Saludar(nombre)
