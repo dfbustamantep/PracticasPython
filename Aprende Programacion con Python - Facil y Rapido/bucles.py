acc0ef6abbf7e1ab8f0f43b615f0c1931aa1c3c7
Daniel Bustamante <vtdanielf@gmail.com>
2024-01-26 09:34:10 +0100

    bucles en python

---
+++ Aprende Programacion con Python - Facil y Rapido/bucles.py
@@ -0,0 +1,20 @@
+
+#bucle WHILE mientras
+i = 0
+while i<10:
+    i = i+1
+    print("El numero es ",i)
+
+#Bucle infinito
+'''while True:
+    print(1)
+'''
+#Bucle for
+lista = {"hamburguesa","perro","pan","salchichas"}
+
+for i in lista:
+    print(i)
+
+for numero in range(0,10):
+    print(numero)
+
