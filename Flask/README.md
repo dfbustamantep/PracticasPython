32e39f2046b563f45dd75f5fc3aaad034e6f1d3a
Daniel Bustamante <vtdanielf@gmail.com>
2024-12-27 23:15:29 -0500

    Templates,condiciones y ciclos con flask

--- Flask/README.md
+++ Flask/README.md
@@ -26,3 +26,28 @@ https://flask.palletsprojects.com/en/stable/
 ```bash
     app = Flask(__name__)
 ```
+- Se deben definir rutas o endpoints para nuestra aplicacion
+```bash
+    @app.route('/home')
+    def hello():
+        return 'Hello World!'
+```
+- Ponemos en funcionamiento el servidor de flask
+```bash
+    app.run(host='0.0.0.0',port=81)
+```
+- Si queremos hacer debuggin debemos poner un parametro mas
+```bash
+    app.run(host='0.0.0.0',port=81,debug=True)
+```
+- Objeto request
+    - Nos permite obtener todos los datos que vienen del cliente
+```bash
+    from flask import request
+```
+- Objeto response
+    - Nos permite obtener todos los datos que vienen del cliente
+```bash
+    from flask import make_response,redirect
+```
+    - Nos permite redireccionar
--- Flask/main.py
+++ Flask/main.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask,request,make_response,redirect,render_template
 
 app = Flask(__name__)
 
-# endpoint punto final o ruta
-@app.route('/home')
-def hello():
-    return 'Hello World!'
+items ={"ITEM 1","ITEM 2","ITEM 3"}
 
+# endpoint punto final o ruta
+@app.route('/index')
+def index():
+    user_ip_information = request.remote_addr
+    response = make_response(redirect("/show_information_address"))
+    response.set_cookie("user_ip_information",user_ip_information)
+    return response
+    #return f"Hola,Como estas? tu direccion IP es la siguiente {user_ip_information}"
+    #return 'Hello World!'
+
+@app.route('/show_information_address')
+def show_information():
+    user_ip =request.cookies.get('user_ip_information')
+    context = {
+        "user_ip":user_ip,
+        "items":items
+    }
+    #return f"Hola que tal,tu direccion ip es {user_ip}"
+                            #Busca en el carpeta templeates el archivo,la vairable de la izquierda es el valor que se esta llamando en el html
+    return render_template('ip_information.html',**context)
 #desde cualquier direccion
-app.run(host='0.0.0.0',port=81)
+app.run(host='0.0.0.0',port=81,debug=True)
